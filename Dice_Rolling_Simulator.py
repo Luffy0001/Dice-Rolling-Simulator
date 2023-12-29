2b97c0ef9cd882c0ba6908c73d0ff7cffe3d75e4
Thomas McGookin <thomas.mcgokin17@gmail.com>
2023-12-29 21:13:04 +0000

    A simple dice rolling simulator

---
+++ Dice_Rolling_Simulator.py
@@ -0,0 +1,7 @@
+import random
+
+dice_1 = random.randint(1, 6)
+dice_2 = random.randint(1, 6)
+
+print(f"you rolled a {dice_1} on dice one and a {dice_2} on dice 2.")
+print(f"The combined number of both dice is {dice_1 + dice_2}.")
